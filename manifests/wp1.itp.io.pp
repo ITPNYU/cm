11b7e1018e7c0f2c008e9f57f997f3989e91ab3d
Karl Ward <karl@ghostghost.net>
2014-12-22 13:16:40 -0500

    PHP bindings --KW

--- manifests/wp1.itp.io.pp
+++ manifests/wp1.itp.io.pp
@@ -1,11 +1,14 @@
 class { 'apache':
   mpm_module => 'prefork'
 }
-class {'::apache::mod::php': }
+class { '::apache::mod::php': }
 
 include '::mysql::server'
 include '::mysql::server::account_security'
 include '::mysql::client'
+class { 'mysql::bindings':
+  php_enable => true
+}
 
 package {'libssh2-php': ensure => 'installed' }
 package {'php5-gd': ensure => 'installed' }
