51580b3efb4dea67eff3729b94a37bf9e4421fc1
Karl Ward <karl@ghostghost.net>
2014-12-22 12:55:24 -0500

    PHP deps --KW

--- manifests/wp1.itp.io.pp
+++ manifests/wp1.itp.io.pp
@@ -3,3 +3,6 @@ class { 'apache': }
 include '::mysql::server'
 include '::mysql::server::account_security'
 include '::mysql::client'
+
+package {'libssh2-php': ensure => 'installed' }
+package {'php5-gd': ensure => 'installed' }
